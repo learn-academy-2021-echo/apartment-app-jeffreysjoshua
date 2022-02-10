0e85819e930b7ce8dfb1c519890cd6c0cfc0353a
LEARN Academy <jeffreysjoshua@gmail.com>
2022-02-10 13:07:19 -0800

    Navigation done

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -8,8 +8,18 @@ import {
 import AboutUs from './pages/AboutUs'
 import LearnMore from './pages/LearnMore'
 import Home from './pages/Home'
+import Header from './components/Header'
+import mockApts from './mockApts.js'
+import AptIndex from './pages/AptIndex'
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      apts: mockApts
+    }
+  }
   render() {
     const {
       logged_in,
@@ -18,24 +28,21 @@ class App extends Component {
       sign_in_route,
       sign_out_route
     } = this.props
+
+    
     console.log("logged_in", logged_in)
     return(
     
-      <>
+      <Router>
+      <Header {...this.props}/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/aptindex" render={(props) => <AptIndex apts={this.state.apts} />} />
+        <Route path="/about" component={AboutUs} />
+        <Route path="/learn" component={LearnMore} />
       <h1>Hola Mundo!</h1>
-      <br /> 
-      <br /> 
-        {logged_in &&
-          <div>
-            <a href={sign_out_route}>Sign Out</a>
-          </div>
-        }
-        {!logged_in &&
-          <div>
-            <a href={sign_in_route}>Sign In</a>
-          </div>
-        }
-      </>
+      </Switch>
+      </Router>
     )
   }
 }
---
+++ app/javascript/components/mockApts.js
@@ -0,0 +1,52 @@
+
+let apts = [
+    {
+      street: "123 Road",
+      city: "Los Angeles",
+      state: "California",
+      manager: "Mike Smith",
+      email: "mikesmith@aol.com",
+      price: "$2000",
+      bedrooms: 2,
+      bathrooms: 1,
+      pets: "no",
+      user_id: 1
+    },
+    {
+        street: "456 Street",
+        city: "Los Angeles",
+        state: "California",
+        manager: "Julie Jones",
+        email: "juliejones@aol.com",
+        price: "$2500",
+        bedrooms: 2,
+        bathrooms: 2,
+        pets: "yes",
+        user_id: 2
+    },
+    {
+        street: "789 Way",
+        city: "San Francisco",
+        state: "California",
+        manager: "Justin Case",
+        email: "justincase@aol.com",
+        price: "$3800",
+        bedrooms: 1,
+        bathrooms: 1,
+        pets: "no",
+        user_id: 3
+    },
+    {
+      street: "987 Place",
+      city: "New York",
+      state: "New York",
+      manager: "Tony Rome",
+      email: "tonyrome@aol.com",
+      price: "$3300",
+      bedrooms: 2,
+      bathrooms: 2,
+      pets: "no",
+      user_id: 4
+    }
+  ]
+  export default apts
