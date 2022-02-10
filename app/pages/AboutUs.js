f9100a9a25d9f07118b368dd21d7302f33cb8093
LEARN Academy <jeffreysjoshua@gmail.com>
2022-02-10 09:51:26 -0800

    sending up another

---
+++ app/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AboutUs extends Component {
+  render() {
+    return(
+      <h3>This is the About Us Page</h3>
+    )
+  }
+}
+
+export default AboutUs
---
+++ app/pages/LearnMore.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class LearnMore extends Component {
+  render() {
+    return(
+      <h3>This is the LearnMore Page</h3>
+    )
+  }
+}
+
+export default LearnMore
